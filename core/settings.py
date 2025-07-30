ea9c961cfc8154b78abf52e065ffad04a8a1c1ed
Asadbek <hoshimovaustin0550@gmail.com>
2025-07-30 10:12:06 +0900

    created .env file

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import decouple
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rz@+11(emkh0-uhq6#h)$gp7-zy-e=pm@g*iyvog1lnt7wnnt*"
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- tests.py
+++ tests.py
@@ -2,18 +2,20 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 
 
-# class Hosttest(LiveServerTestCase):
-#     def testhomepage(self):
-#         driver = webdriver.Chrome()
-#
-#         driver.get('http://127.0.0.1:8000/')
-#
-#         time.sleep(5)
-#
-#         assert "Hello, world!" in driver.title
+class Hosttest(LiveServerTestCase):
+    def testhomepage(self):
+
+        driver = webdriver.Chrome()
+
+        driver.get('http://127.0.0.1:8000/')
+
+        time.sleep(5)
+
+        assert "Hello, world!" in driver.title
 
 class LoginFormTest(LiveServerTestCase):
     def testform(self):
