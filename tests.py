bcdb3b302cca35fdd07fa7f55d0601d96a5fe6cc
Asadbek <hoshimovaustin0550@gmail.com>
2025-07-30 09:17:58 +0900

    test using chromedriver

---
+++ tests.py
@@ -0,0 +1,10 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+import time
+
+class Hosttest(LiveServerTestCase):
+    def testhomepage(self):
+        driver = webdriver.Chrome()
+
+        driver.get('http://127.0.0.1:8000/')
+        assert "Hello, world!" in driver.title
